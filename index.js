d64fa0fb352da2f6dc5ae24e825d039b4006694e
David Cha <davidcha17@yahoo.com>
2020-01-28 20:29:19 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+
+
+var customerName = "bob"
+
+function name(){
+     return customerName
+}
+
+function upperCaseCustomerName(){
+    customerName =  customerName.toUpperCase()
+}
+
+var bestCustomer
+
+function setBestCustomer(){
+    return bestCustomer = 'not bob'
+}
+
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+
+const leastFavoriteCustomer = 'nothing'
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "whip"
+}
